5aa9c4ac8a568e2500d47313969246afa363a32a
Leessang <992275294@qq.com>
2018-01-05 16:01:52 +0800

    创建KNCollectionViewCell

---
+++ KNCollectionFlowLayout/KNViews/KNCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  KNCollectionViewCell.h
+//  KNCollectionFlowLayout
+//
+//  Created by 刘凡 on 2018/1/5.
+//  Copyright © 2018年 leesang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KNCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ KNCollectionFlowLayout/KNViews/KNCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  KNCollectionViewCell.m
+//  KNCollectionFlowLayout
+//
+//  Created by 刘凡 on 2018/1/5.
+//  Copyright © 2018年 leesang. All rights reserved.
+//
+
+#import "KNCollectionViewCell.h"
+
+@implementation KNCollectionViewCell
+
+@end
