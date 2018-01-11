11946fb7fd20d6a9ac338fa000076ad189f74018
Leessang <992275294@qq.com>
2018-01-11 15:16:14 +0800

    完成cell 编写

--- KNCollectionFlowLayout/KNViews/KNCollectionViewCell.h
+++ KNCollectionFlowLayout/KNViews/KNCollectionViewCell.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+
+///屏幕的宽高
+#define SCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
+#define SCREEN_HEIGHT ([[UIScreen mainScreen] bounds].size.height)
+
 //Cell的高度
 #define CELL_HEIGHT (SCREEN_WIDTH*0.24) //普通高度
 ///置顶高度
