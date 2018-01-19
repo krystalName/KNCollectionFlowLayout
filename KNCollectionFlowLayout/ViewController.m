77965e2a7806e159cb3da21132546d804134fecb
Leessang <992275294@qq.com>
2018-01-19 15:00:04 +0800

    完成代码编写

--- KNCollectionFlowLayout/ViewController.m
+++ KNCollectionFlowLayout/ViewController.m
@@ -53,15 +53,66 @@
     return 1;
 }
 
+
+
 // 每个section中得items个数
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     return self.dataSourcesArray.count +1 ;
+}
+
+// cell
+- (KNCollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *identifier = NSStringFromClass([KNCollectionViewCell class]);
+    KNCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+    
+    cell.tag = indexPath.row;
+    [cell setIndex:(indexPath.row)];
+    
+    if(indexPath.row == 0){
+        cell.imageView.image = nil;
+    }else{
+        if(indexPath.row == 1){
+            [cell revisePositionAtFirstCell];
+        }
+        cell.titleLabel.text = @"好看的海报";
+        cell.descLabel.text = @"爱情 | 亲情";
+        cell.bottomLabel.text = @"好看的外表千篇一律";
+        cell.imageView.image = [UIImage imageNamed:@"116"];
+    }
+    return cell;
+}
+
+
+// 每个item的frame.size
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    if(indexPath.row == 0){
+        return CGSizeMake(MIN(SCREEN_HEIGHT, SCREEN_WIDTH), HEADER_HEIGHT);
+    }else if(indexPath.row == 1){
+        return CGSizeMake(MIN(SCREEN_HEIGHT, SCREEN_WIDTH), CELL_CURRHEIGHT);
+    }else{
+        return CGSizeMake(MIN(SCREEN_HEIGHT, SCREEN_WIDTH), CELL_HEIGHT);
+    }
+}
+
+// 点击事件
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    CGFloat offset = ceil(DRAG_INTERVAL * (indexPath.row-1));
+    if (ceil(collectionView.contentOffset.y) != offset) {
+        // 滑动动最顶部
+        self.layout.currentCount = indexPath.row;
+        [collectionView setContentOffset:CGPointMake(0, offset) animated:YES];
+    } else {
+        // 点击事件
+        NSLog(@"点击了第N%@个",@(indexPath.row));
+    }
 }
 
 
 - (void)scrollViewDidScrollToTop:(UIScrollView *)scrollView {
     self.layout.currentCount = 1;
 }
+
 
 
 #pragma mark - Init Views
